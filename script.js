3abd3c1604c8f8e661803ccf9885511e60acdb87
Pavel Merekin <merekin33@gmail.com>
2024-04-03 20:41:56 +0800

    Add script without animation

---
+++ script.js
@@ -0,0 +1,43 @@
+const gameContainer = document.querySelector(".container"),
+  userResult = document.querySelector(".user_result img"),
+  cpuResult = document.querySelector(".cpu_result img"),
+  result = document.querySelector(".result"),
+  optionImages = document.querySelectorAll(".option_image");
+
+optionImages.forEach((image, index) => {
+  image.addEventListener("click", (e) => {
+    image.classList.add("active");
+
+    optionImages.forEach((image2, index2) => {
+      index !== index2 && image2.classList.remove("active");
+    });
+
+    let imageSrc = e.target.src;
+    userResult.src = imageSrc;
+
+    let randomNumber = Math.floor(Math.random() * 3);
+    let cpuImages = ["imgs/rock.svg", "imgs/paper.svg", "imgs/scissors.svg"];
+    cpuResult.src = cpuImages[randomNumber];
+
+    let cpuValue = ["R", "P", "S"][randomNumber];
+    let userValue = ["R", "P", "S"][index];
+
+    let outcomes = {
+      RR: "Draw",
+      RP: "Cpu",
+      RS: "User",
+      PP: "Draw",
+      PR: "User",
+      PS: "Cpu",
+      SS: "Draw",
+      SR: "Cpu",
+      SP: "User",
+    };
+
+    let outcomeValue = outcomes[userValue + cpuValue];
+
+    result.textContent =
+      userValue === cpuValue ? "Match Draw" : `${outcomeValue} Won!!`;
+    console.log(outcomeValue);
+  });
+});
